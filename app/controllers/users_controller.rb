fbaa8089f2bb87f18a2adb8518a05ec3571c09c2
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-07-15 15:30:50 +0700

    crud user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,70 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new create)
+  before_action :load_user, except: %i(new create index)
+  before_action :admin_user, except: %i(show)
+
+  def index
+    @users = User.page(params[:page]).per Settings.num_user
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t("controllers.user.success_create")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t("controllers.user.success_edit")
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t("controllers.user.delete_s")
+    else
+      flash[:danger] = t("controllers.user.delete_err")
+    end
+    redirect_to users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit User::USER_PARAMS
+  end
+
+  def load_user
+    @user = User.find_by_id params[:id]
+    return if @user
+    flash[:danger] = t("controllers.user.not_exits")
+    redirect_to root_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t("controllers.user.please_ln")
+    redirect_to login_url
+  end
+
+  def admin_user
+    return if current_user.manager?
+    flash[:danger] = t("controllers.user.no_role")
+    redirect_to root_url
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  USER_PARAMS = %i(phone address name username role password password_confirmation).freeze
+  enum role: {employee: 0, manager: 1}
+
   has_many :bills, dependent: :destroy
 
   validates :username, format: {with: Settings.VALID_EMAIL_REGEX},
   presence: true, length: {maximum: Settings.max_username},
   uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: Settings.min_password}, allow_nil: true
+  validates :name, presence: true, length: {minimum: Settings.min_name, maximum: Settings.max_name}
+  validates :phone, presence: true, length: {minimum: Settings.min_phone, maximum: Settings.max_phone}
+  validates :address, presence: true, length: {minimum: Settings.min_address, maximum: Settings.max_address}
 
   has_secure_password
 
