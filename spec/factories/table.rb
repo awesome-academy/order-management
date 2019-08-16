4f10c421c8959329083a3ce67033966030e6167a
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-08-16 08:37:22 +0700

    rspec controllers

--- spec/factories/table.rb
+++ spec/factories/table.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   factory :table do |f|
     f.number {FFaker::Name.name}
     f.description {FFaker::Lorem.paragraph}
-    f.amount_chair {2}
+    trait :amount_chair do
+      double_table {Settings.table_type.double_table}
+    end
     trait :status do
       active {Settings.table_status.active}
     end
